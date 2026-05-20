c688a5e21c4fc1405f533c5a83b61020e64ca650
jlaventure <jlaventure3679@floridapoly.edu>
2026-05-20 18:35:00 +0000

    create first program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,23 @@
+/*
+My first C program
+I will finish it on Wednesday.
+
+*/
+
+/*multiline comment*/
+
+// one-line comment
+
+
+// Preprocessor directive (starts with #)
+// stdio - standard input output
+// .h - header file
+#include <stdio.h>
+
+// Create the main function
+// <return type> <name> (<parameters>)
+int main(void) {
+    printf("Welcome to COP2271!\n"); // \n - new line (escape character)
+    printf("Are\nyou\nready\nto\ncode?");
+    return 0;
+}

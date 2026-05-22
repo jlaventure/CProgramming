f0c37fa3522b0c4f2e7f5d72f8c48998bc72b71f
jlaventure <jlaventure3679@floridapoly.edu>
2026-05-22 15:09:21 +0000

    secondToHMS practice

--- 03_secondsToHMS/secondToHMS.c
+++ 03_secondsToHMS/secondToHMS.c
@@ -7,18 +7,17 @@ The program converts the given number to HMS.
 
 #include <stdio.h>
 
-int main(void) {
-    int seconds = 0;
+void secondsToHMS(int seconds) {
     int minutes = 0;
     int hours = 0;
     int remaining_seconds = 0;
-    int remaining_minutes = 0;
+    int remaining_minutes = 0; 
 
-    // Get the number of seconds
-    puts("Enter the number of seconds");
-    scanf("%d", &seconds);
-
-    // Calculate the number of minutes
+     if (seconds , 0) {
+        puts("The number of seconds cannot be negative:");
+    }
+    else{
+         // Calculate the number of minutes
     minutes = seconds / 60;
     remaining_seconds = seconds % 60; // seconds - (minutes * 60);
 
@@ -27,9 +26,23 @@ int main(void) {
     remaining_minutes = minutes % 60;
 
     // Print the result
-    printf("%d seconds is", seconds);
+    printf("%d seconds is ", seconds);
     printf("%d hours and ", hours);
     printf("%d minutes and %d seconds\n", remaining_minutes, remaining_seconds);
+    }
+}
+
+int main(void) {
+    int seconds = 0;
+    
+
+    // Get the number of seconds
+    puts("Enter the number of seconds");
+    scanf("%d", &seconds);
+
+    // Call the function
+    secondsToHMS(seconds);
+
 
 
 
